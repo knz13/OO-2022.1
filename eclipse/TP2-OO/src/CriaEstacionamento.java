642e80b054c26d6226332f281d2d31c79b0153de
Brunna Louise Martins <brunna.louisemr@gmail.com>
2022-08-31 14:19:03 -0300

    mexendo no estacionamento
    Alteração da classe Estacionamento e de seus métodos, criação de protótipo de função para criação de estacionamento.

---
+++ eclipse/TP2-OO/src/CriaEstacionamento.java
@@ -0,0 +1,14 @@
+
+
+public class CriaEstacionamento {
+
+	public static void main(String[] args) {
+		System.out.println("Você selecionou a opção para criar um novo estacionamento!");
+		Estacionamento estacionamento = new Estacionamento();
+
+		boolean nomeEstacionamentoValido;
+		
+		
+	}
+	
+}
--- eclipse/TP2-OO/src/Estacionamento.java
+++ eclipse/TP2-OO/src/Estacionamento.java
@@ -1,7 +1,8 @@
+import javax.swing.JOptionPane;
 
 public class Estacionamento {
 
-	// atributos basicos de estacionamento
+	// atributos de estacionamento
 	private String nomeEstacionamento;
 	private double fracaoQuinzeMinutos;
 	private double descontoHoraCheia;
@@ -13,13 +14,25 @@ public class Estacionamento {
 	private Evento evento; // essa classe precisa ter acesso a classe de eventos
 	private Horas horarioFuncionamento;
 	private Horas funcionamentoNoturno;
+	
+	// o construtor para criar novos objetos do tipo estacionamento
+	// faltam as horas! ainda tenho que mexer na classe de horas!
+	// public Estacionamento(String nomeEstacionamento, double fracaoQuinzeMinutos, double descontoHoraCheia, double diariaDiurna, double diariaNoturna, double valorMensalista, double retornoContratante, int capacidadeEstacionamento) { 
+	// isso aqui ta meio paia	
+	// }
 
 	public String getNomeEstacionamento() {
 		return nomeEstacionamento;
 	}
 
-	public void setNomeEstacionamento(String nomeEstacionamento) {
-		this.nomeEstacionamento = nomeEstacionamento;
+	public boolean setNomeEstacionamento(String nomeEstacionamento) {
+		if (nomeEstacionamento == null) {
+			JOptionPane.showMessageDialog(null, "Nome do estacionamento não pode ser um valor nulo!");
+			return false;
+		} else {
+			this.nomeEstacionamento = nomeEstacionamento;
+			return true;
+		}
 	}
 
 	public double getFracaoQuinzeMinutos() {
