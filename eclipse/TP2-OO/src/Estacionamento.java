ca4e2bce8fa8a90e99310e88d0c8870569680fce
Brunna Louise Martins <brunna.louisemr@gmail.com>
2022-09-01 17:58:09 -0300

    prototipo estacionamento
    Aprimoramento da classe de estacionamentos e melhoria da função de criar um novo objeto de tipo estacionamento.

--- eclipse/TP2-OO/src/Estacionamento.java
+++ eclipse/TP2-OO/src/Estacionamento.java
@@ -1,8 +1,7 @@
-import javax.swing.JOptionPane;
 
 public class Estacionamento {
 
-	// atributos de estacionamento
+	// atributos basicos de estacionamento
 	private String nomeEstacionamento;
 	private double fracaoQuinzeMinutos;
 	private double descontoHoraCheia;
@@ -14,25 +13,13 @@ public class Estacionamento {
 	private Evento evento; // essa classe precisa ter acesso a classe de eventos
 	private Horas horarioFuncionamento;
 	private Horas funcionamentoNoturno;
-	
-	// o construtor para criar novos objetos do tipo estacionamento
-	// faltam as horas! ainda tenho que mexer na classe de horas!
-	// public Estacionamento(String nomeEstacionamento, double fracaoQuinzeMinutos, double descontoHoraCheia, double diariaDiurna, double diariaNoturna, double valorMensalista, double retornoContratante, int capacidadeEstacionamento) { 
-	// isso aqui ta meio paia	
-	// }
 
 	public String getNomeEstacionamento() {
 		return nomeEstacionamento;
 	}
 
-	public boolean setNomeEstacionamento(String nomeEstacionamento) {
-		if (nomeEstacionamento == null) {
-			JOptionPane.showMessageDialog(null, "Nome do estacionamento não pode ser um valor nulo!");
-			return false;
-		} else {
-			this.nomeEstacionamento = nomeEstacionamento;
-			return true;
-		}
+	public void setNomeEstacionamento(String nomeEstacionamento) {
+		this.nomeEstacionamento = nomeEstacionamento;
 	}
 
 	public double getFracaoQuinzeMinutos() {
