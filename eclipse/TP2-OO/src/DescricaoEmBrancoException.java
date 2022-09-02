a64f9ec26e205fc5a005dedefde3369846b21012
knz13 <otaviomaya@gmail.com>
2022-09-02 15:35:52 -0300

    atualizando exceções

--- eclipse/TP2-OO/src/DescricaoEmBrancoException.java
+++ eclipse/TP2-OO/src/DescricaoEmBrancoException.java
@@ -4,4 +4,7 @@ public class DescricaoEmBrancoException extends RuntimeException {
     public DescricaoEmBrancoException(String errorMsg,Throwable err){
         super(errorMsg,err);
     }
+    public DescricaoEmBrancoException(String errorMsg){
+        super(errorMsg);
+    }
 }
